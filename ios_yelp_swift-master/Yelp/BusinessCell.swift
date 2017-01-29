12f0755a8c3ec72083a6271b3e98970bc6a9cf5a
ROBERT HERNANDEZ <robert.hernandez@coupa.com>
2017-01-28 18:53:04 -0800

    Yelp Setup Done

---
+++ ios_yelp_swift-master/Yelp/BusinessCell.swift
@@ -0,0 +1,48 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Rob Hernandez on 1/28/17.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    @IBOutlet weak var catagoriesLabel: UILabel!
+    @IBOutlet weak var reviewLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    var business: Business!{
+        didSet{
+            nameLabel.text = business.name
+            reviewLabel.text = "\(business.reviewCount) Reviews"
+            distanceLabel.text = business.distance
+            addressLabel.text = business.categories
+            catagoriesLabel.text = business.categories
+            ratingImageView.setImageWith(business.ratingImageURL!)
+            thumbImageView.setImageWith(business.imageURL!)
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        // Round the images
+        thumbImageView.layer.cornerRadius = 3
+        thumbImageView.clipsToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
